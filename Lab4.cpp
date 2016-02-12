8ab5a5deed12754c23e70ca881b097519b1a3726
fmaldonados <felixmaldonado@unitec.edu>
2016-02-12 14:34:27 -0600

    actualizar

--- Lab4.cpp
+++ Lab4.cpp
@@ -4,24 +4,32 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+void imprimir_cubo(int***, int);
 int main(int argc, char*argv[]){
 	srand(time(NULL));
-	int azar = (rand()% 100 - 500); 
-	int size = 12;
-
-	int*** matriz = new int**[size];
-
-	for (int i = 0; i < size; i++){
-		for (int j = 0; j < size; j++){
-			matriz[i][j] = new int*[size];
-		}	
-	}
-
-	for (int i = 0; i < size; i++){
-		for (int j = 0; j < size; j++){
-			matriz[i][j] = int azar = (rand()% 12 + 0)
-		}
-	}
-	
 	return 0;
 }
+
+void imprimir_cubo(int*** cubo, int size){
+	for(int i =0; i<size; i++){
+		cout<<"piso: "<< i<<endl;
+		for(int j=0;j<size; j++){
+			for(int k=0; i<size; k++){
+				cout << cubo[i][j][k];
+			}
+			cout<<endl;
+		}
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
