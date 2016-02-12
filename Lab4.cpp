40a559e98b4f129d51a8c869e729c0514ff34caf
alexycruz1 <xytojr@gmail.com>
2016-02-12 13:58:03 -0600

    declaracion matriz

--- Lab4.cpp
+++ Lab4.cpp
@@ -7,6 +7,14 @@ using std::endl;
 int main(int argc, char*argv[]){
 	srand(time(NULL));
 	int Azar = (rand()% 100 - 500); 
+	int SIZE = 12;
 
+	int*** matriz = new int**[SIZE];
+	for (int i = 0; i < SIZE; i++){
+		for (int j = 0; j < SIZE; j++){
+			
+		}
+		
+	}
 	return 0;
 }
