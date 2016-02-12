7fdfe5a06b51e80396c13a82082b7600c57738cc
fmaldonados <felixmaldonado@unitec.edu>
2016-02-12 15:29:17 -0600

    menu de ataques

--- Lab4.cpp
+++ Lab4.cpp
@@ -8,17 +8,42 @@ using std::endl;
 
 void crear_llenar(int***);
 void imprimir_cubo(int*** ,int);
-
+int tipo_ataque(int,int);
 int main(int argc, char*argv[]){
 	srand(time(NULL));
 	int size = 12;
-	
+	int wave1=3,wave2=3, expansive1=3,expansive2=3;
+
 	int*** matriz = new int**[size];
 	crear_llenar(matriz);
-	imprimir_cubo(matriz,size);
+	
+	
 	return 0;
 }
 
+void tipo_ataque(int wave, int expansive){
+	int seleccion;
+	bool seguir=true;
+	while(seguir){
+		cout<<"Seleccione el tipo de ataque: "<<endl
+		<<"1-Normal"<<endl
+		<<"2-Wave "<<"Quedan"<< wave<<" Por usar."<<endl
+		<<"3-Expansive "<< "Quedan "<< expansive << "Por usar"<<endl;
+		cin>> seleccion;
+	
+		if(seleccion == 2 && wave ==0){
+			cout<< "No quedan mas ataques del tipo wave."
+		}else{
+			if(seleccion == 3 && expansive == 0){
+	
+			}else{
+				seguir=false;
+			}
+	
+		}
+	}
+	return seleccion;
+}
 void imprimir_cubo(int*** cubo, int size){
         for(int i =0; i<size; i++){
                 cout<<"piso: "<< i<<endl;
